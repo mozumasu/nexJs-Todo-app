1f3d110725f9f6196522c1c5e786e58540d8ac59
mozumasu <ouri1229@gmail.com>
2023-09-06 08:31:12 +0900

    APIから取得した配列をTodoListへ表示

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -1,22 +1,25 @@
+import { TaskType } from '@/page/types';
 import React from 'react';
 
-const TodoList = () => {
+interface TodoListProps {
+  tasks: TaskType[];
+}
+
+const TodoList = ({ tasks }: TodoListProps) => {
   return (
     <ul className="space-y-3">
-      <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
-        <span>散歩</span>
-        <div>
-          <button className="text-green-500 mr-3">Edit</button>
-          <button className="text-red-300 mr-1">Delete</button>
-        </div>
-      </li>
-      <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
-        <span>散歩</span>
-        <div>
-          <button className="text-green-500 mr-3">Edit</button>
-          <button className="text-red-300 mr-1">Delete</button>
-        </div>
-      </li>
+      {tasks.map((task) => (
+        <li
+          key={task.id}
+          className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow"
+        >
+          <span>{task.text}</span>
+          <div>
+            <button className="text-green-500 mr-3">Edit</button>
+            <button className="text-red-300 mr-1">Delete</button>
+          </div>
+        </li>
+      ))}
     </ul>
   );
 };
