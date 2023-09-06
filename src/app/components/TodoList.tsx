3ba0f9764a862fecbe5c6aa4b96b7624d1c0476a
mozumasu <ouri1229@gmail.com>
2023-09-06 18:44:11 +0900

    TodoList表示API

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -1,5 +1,6 @@
 import { TaskType } from '@/page/types';
 import React from 'react';
+import Task from './Task';
 
 interface TodoListProps {
   tasks: TaskType[];
@@ -9,16 +10,7 @@ const TodoList = ({ tasks }: TodoListProps) => {
   return (
     <ul className="space-y-3">
       {tasks.map((task) => (
-        <li
-          key={task.id}
-          className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow"
-        >
-          <span>{task.text}</span>
-          <div>
-            <button className="text-green-500 mr-3">Edit</button>
-            <button className="text-red-300 mr-1">Delete</button>
-          </div>
-        </li>
+        <Task key={task.id} task={task} />
       ))}
     </ul>
   );
